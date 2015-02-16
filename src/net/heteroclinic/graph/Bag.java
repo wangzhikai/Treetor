3e39e2164e30e7dc0fbc136534a0209457807c79
Zhikai Wang <zhikai@heteroclinic.net>
2015-02-16 11:26:14 -0800

    Merge treetorplus3

--- src/net/heteroclinic/graph/Bag.java
+++ src/net/heteroclinic/graph/Bag.java
@@ -1,4 +1,12 @@
+/*
+ * The author Zhikai Wang/www.heteroclinic.net reserves all rights of this file.
+ * (c) 2012  Zhikai Wang/www.heteroclinic.net
+ *  
+ */
+
 package net.heteroclinic.graph;
+
+
 
 public class Bag {
 	
--- src/net/heteroclinic/graph/Edge.java
+++ src/net/heteroclinic/graph/Edge.java
@@ -1,3 +1,9 @@
+/*
+ * The author Zhikai Wang/www.heteroclinic.net reserves all rights of this file.
+ * (c) 2012  Zhikai Wang/www.heteroclinic.net
+ *  
+ */
+
 package net.heteroclinic.graph;
 
 public class Edge { 
